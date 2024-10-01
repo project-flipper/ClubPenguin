67815ee4bdbf12b09ae0c5331facf380d4908c94
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-30 20:21:40 -0600

    Add log documentation

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -8,6 +8,11 @@ export enum LogLevel {
     CRITICAL = ERROR
 }
 
+/**
+ * Returns a human-readable string representation of a log level.
+ * @param level The log level.
+ * @returns The string representation.
+ */
 export function getLevelName(level: LogLevel): string {
     switch (level) {
         case LogLevel.TRACE:
@@ -27,6 +32,10 @@ export function getLevelName(level: LogLevel): string {
 
 export const DEFAULT_LEVEL = LogLevel.WARN;
 
+/**
+ * Represents a record waiting to be logged.
+ * You typically do not create these and instead employ a {@link Logger}.
+ */
 export class LogRecord {
     msg: string;
     args: any[];
@@ -45,11 +54,15 @@ export class LogRecord {
         this.mappings = {
             'msg': this.msg,
             'name': logger.name,
-            'now': new Date().toISOString()
+            'now': new Date().toISOString(),
+            'level': 'UNKNOWN'
         };
     }
 }
 
+/**
+ * Provides record formatting before logging.
+ */
 export class Formatter {
     msgFormat: string;
     afterMsg: any[];
@@ -59,7 +72,15 @@ export class Formatter {
         this.afterMsg = afterMsg;
     }
 
+    /**
+     * Transforms a record into a sequence of arguments for a {@link console} log method.
+     * @param level The current log level.
+     * @param record The record to be formatted.
+     * @returns A sequence of ordered arguments to be fed into any {@link console} logging methods.
+     */
     getConsoleArgs(level: LogLevel, record: LogRecord): any[] {
+        record.mappings.level = record.mappings.level ?? getLevelName(level);
+
         let msg = this.msgFormat;
 
         for (let key in record.mappings) {
@@ -70,6 +91,10 @@ export class Formatter {
     }
 }
 
+/**
+ * Identical to {@link Formatter} except a color argument gets inserted into the console arguments.
+ * This color argument changes based on the log level, and can be customized as needed.
+ */
 export class ColorLevelFormatter extends Formatter {
     colors: Record<LogLevel, string>;
     colorArgIndex: number;
@@ -82,8 +107,6 @@ export class ColorLevelFormatter extends Formatter {
     }
 
     getConsoleArgs(level: LogLevel, record: LogRecord): any[] {
-        record.mappings.level = record.mappings.level ?? getLevelName(level);
-
         this.afterMsg[this.colorArgIndex] = this.colors[level] ?? '';
 
         return super.getConsoleArgs(level, record);
@@ -92,6 +115,10 @@ export class ColorLevelFormatter extends Formatter {
 
 export const DEFAULT_FORMAT = new Formatter('[%c{now}%c] [%c{name}%c] %c{msg}%c', ['color:blue', '', 'color: red', '', 'color:grey', '']);
 
+/**
+ * The factory for record creation and logging.
+ * You typically do not instantiate these directly, as {@link getLogger} will handle mapping and parents for you.
+ */
 export class Logger {
     _name: string;
     _level: LogLevel;
@@ -101,15 +128,26 @@ export class Logger {
         this._name = name;
     }
 
+    /**
+     * The name associated with this logger.
+     * This is the full path to which this logger is mapped at.
+     */
     get name(): string {
         return this._name;
     }
 
+    /**
+     * The parent of this logger.
+     */
     get parent(): Logger {
         let parent = this.name.split('.');
         return getLogger(parent.slice(0, -1).join('.'));
     }
 
+    /**
+     * The level at which this logger can log at.
+     * If none is set, gets the value from the parent logger.
+     */
     get level(): LogLevel {
         return this._level ?? this.parent.level;
     }
@@ -118,12 +156,25 @@ export class Logger {
         this._level = level;
     }
 
+    /**
+     * The formatter associated with this logger.
+     * If none is set, gets the value from the parent logger.
+     */
     get formatter(): Formatter {
         return this._formatter ?? this.parent.formatter;
     }
 
     set formatter(formatter: Formatter) {
         this._formatter = formatter;
+    }
+
+    /**
+     * Gets or creates a logger that descends from this logger.
+     * @param name The name of the child logger.
+     * @returns The child logger.
+     */
+    getChild(name: string): Logger {
+        return getLogger(`${this.name}.${name}`);
     }
 
     _log(level: LogLevel, record: LogRecord): void {
@@ -150,32 +201,62 @@ export class Logger {
         }
     }
 
+    /**
+     * Creates and logs a record given a level.
+     * @param level The level to log at.
+     * @param messages The logging messages.
+     */
     log(level: LogLevel, ...messages: any): void {
         let record = new LogRecord(this, messages);
         this._log(level, record);
     }
 
+    /**
+     * Logs a record at level TRACE.
+     * @param messages The logging messages.
+     */
     trace(...messages: any): void {
         this.log(LogLevel.TRACE, ...messages);
     }
 
+    /**
+     * Logs a record at level DEBUG.
+     * @param messages The logging messages.
+     */
     debug(...messages: any): void {
         this.log(LogLevel.DEBUG, ...messages);
     }
 
+    /**
+     * Logs a record at level INFO.
+     * @param messages The logging messages.
+     */
     info(...messages: any): void {
         this.log(LogLevel.INFO, ...messages);
     }
 
+    /**
+     * Logs a record at level WARN.
+     * @param messages The logging messages.
+     */
     warn(...messages: any): void {
         this.log(LogLevel.WARN, ...messages);
     }
 
+    /**
+     * Logs a record at level ERROR.
+     * @param messages The logging messages.
+     */
     error(...messages: any): void {
         this.log(LogLevel.ERROR, ...messages);
     }
 }
 
+/**
+ * The root logger.
+ * You should not instantiate this class yourself as this is just used as the fallback parent from which all loggers derive.
+ * It is not recommended to alter the values of this logger unless you intend to make changes globally as a last resort. Avoid it otherwise.
+ */
 class RootLogger extends Logger {
     constructor() {
         super('root');
@@ -199,6 +280,9 @@ class RootLogger extends Logger {
         this._formatter = formatter;
     }
 
+    /**
+     * Undefined as a root logger has no parent.
+     */
     get parent(): undefined {
         return;
     }
@@ -207,9 +291,32 @@ class RootLogger extends Logger {
 const mapping: Map<string, Logger> = new Map();
 export const root = new RootLogger();
 
+/**
+ * Gets or creates a logger.
+ * @param name The name of the logger.
+ * @returns The logger. If no name was provided then returns the root logger.
+ */
+export function getLogger(name?: string): Logger | RootLogger;
+/**
+ * Gets the root logger.
+ * @returns The root logger.
+ */
 export function getLogger(): RootLogger;
+/**
+ * Gets the root logger.
+ * @returns The root logger.
+ */
 export function getLogger(name: ''): RootLogger;
+/**
+ * Gets the root logger.
+ * @returns The root logger.
+ */
 export function getLogger(name: 'root'): RootLogger;
+/**
+ * Gets or creates a logger.
+ * @param name The name of the logger.
+ * @returns The logger.
+ */
 export function getLogger(name: string): Logger;
 export function getLogger(name?: string): Logger | RootLogger {
     if (!name || name == 'root') {
