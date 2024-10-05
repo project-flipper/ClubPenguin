ccadd5ecaa9a2a857223bbf95e0f732b6d386a0e
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-10-04 20:56:29 -0600

    Add more localization features

--- play/translations/index.ts
+++ play/translations/index.ts
@@ -26,7 +26,7 @@ void i18n.use(initReactI18next).use(LanguageDetect).init({
 	interpolation: {
 		escapeValue: false, // not needed for react as it escapes by default
 	},
-	compatibilityJSON: 'v3',
+	compatibilityJSON: 'v4',
 } as InitOptions);
 
 export default i18n;
