948ff5e93b6094692637d37a0008184904a13cbe
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-10-05 22:56:25 -0600

    Localize trial banner

--- play/components/TrialBanner.tsx
+++ play/components/TrialBanner.tsx
@@ -1,33 +1,38 @@
 import React, { useContext, useEffect, useRef } from "react";
+import { Trans, useTranslation } from "react-i18next";
 
 import TrialDaysLeftContext from "../context/TrialDaysLeftContext";
 
 export default () => {
     let containerRef = useRef<HTMLDivElement>(null);
-    let textRef = useRef<HTMLDivElement>(null);
-    let textAltRef = useRef<HTMLDivElement>(null);
 
     let [ daysLeft ] = useContext(TrialDaysLeftContext);
 
     useEffect(() => {
-        if (daysLeft > 1 && containerRef.current) {
+        if (daysLeft >= 1 && containerRef.current) {
             containerRef.current.style.transition = "height 1s";
             containerRef.current.style.height = "28px";
+        } else if (containerRef.current) {
+            containerRef.current.style.height = "0px";
         }
     }, [daysLeft]);
+
+    let { i18n } = useTranslation();
+    let currentLanguage = i18n.resolvedLanguage || i18n.language;
 
     return (
         <div id="TrialBannerContainer" ref={containerRef}>
             {
                 daysLeft > 0 &&
                 <div id="TrialBanner">
-                    <div id="TrialBannerBtn"><a id="TrialBannerLink" href="/" target="_blank"></a></div>
-                    {
-                        daysLeft > 1 ?
-                        <div id="TrialText" className="no_translate" ref={textRef}>Having fun? You have <b>{daysLeft} Days</b> left in your membership</div>
-                        :
-                        <div id="TrialText2" className="no_translate" ref={textAltRef}>Having Fun? Today is the <b>last day</b> of your free membership</div>
-                    }
+                    <div id="TrialBannerBtn" className={currentLanguage != "en" ? `lang-${currentLanguage}` : undefined}>
+                        <a id="TrialBannerLink" href="/" target="_blank"></a>
+                    </div>
+                    <div id="TrialText" className="no_translate">
+                        <Trans i18nKey="trialbanner" count={daysLeft}>
+                            Today is the <b>last day</b> of your free membership.
+                        </Trans>
+                    </div>
                     <div style={{ clear: "both" }}></div>
                 </div>
             }
