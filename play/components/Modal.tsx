764c3ee23a8601ef31bc749fe883ddf9eae4d352
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-10-12 06:50:13 -0600

    Fix modal fade animation

--- play/components/Modal.tsx
+++ play/components/Modal.tsx
@@ -1,7 +1,6 @@
-import React, { useCallback, useEffect } from "react";
+import React, { PropsWithChildren, createRef, useEffect, useState } from "react";
 
 interface ModalProps {
-    children?: React.ReactNode;
     show?: boolean,
     closeModal?: () => void;
     overlayOpacity?: number;
@@ -9,79 +8,69 @@ interface ModalProps {
     fadeDuration?: number;
 }
 
-export default ({ children, closeModal, show = true, overlayOpacity = 0.8, centerOnResize = true, fadeDuration = 250 }: ModalProps) => {
-    let ref = React.createRef<HTMLDivElement>();
-    let overlayRef = React.createRef<HTMLDivElement>();
+export default ({ children, closeModal, show = true, overlayOpacity = 0.8, centerOnResize = true, fadeDuration = 250 }: PropsWithChildren<ModalProps>) => {
+    let [ isHidden, setHidden ] = useState(!show);
 
-    let fadeOutModal = useCallback(() => {
-        if (ref.current) {
-            ref.current.style.transition = `opacity ${fadeDuration}ms easeOut`;
-            ref.current.style.opacity = "0";
-            setTimeout(() => closeModal && closeModal(), fadeDuration);
-        }
-        if (overlayRef.current) {
-            overlayRef.current.style.transition = `opacity ${fadeDuration}ms easeOut`;
-            overlayRef.current.style.opacity = "0";
-        }
-        closeModal && closeModal();
-    }, [closeModal]);
+    let ref = createRef<HTMLDivElement>();
+    let innerRef = createRef<HTMLDivElement>();
 
     useEffect(() => {
         if (show) {
+            document.body.classList.add('modal-active');
             if (ref.current) {
-                ref.current.style.transition = `opacity ${fadeDuration}ms easeIn`;
+                ref.current.style.transition = `opacity ${fadeDuration}ms`;
                 ref.current.style.opacity = "1";
             }
-            if (overlayRef.current) {
-                overlayRef.current.style.transition = `opacity ${fadeDuration}ms easeIn`;
-                overlayRef.current.style.opacity = overlayOpacity.toString();
+            setHidden(false);
+        } else {
+            document.body.classList.remove('modal-active');
+            if (ref.current) {
+                ref.current.style.transition = `opacity ${fadeDuration}ms`;
+                ref.current.style.opacity = "0";
+                setTimeout(() => {
+                    if (closeModal) closeModal();
+                    setHidden(true);
+                }, fadeDuration);
             }
-        } else fadeOutModal();
-    }, [show]);
+        }
+    }, [show, ref, closeModal, fadeDuration]);
 
     useEffect(() => {
         let callback = () => {
-            if (centerOnResize && ref.current) {
-                if (ref.current) {
-                    const element = ref.current;
-                    const elementWidth = element.offsetWidth;
-                    const elementHeight = element.offsetHeight;
-                    const windowWidth = window.innerWidth;
-                    const windowHeight = window.innerHeight;
-                    const scrollTop = window.scrollY;
-                    // calculate center
-                    let left = (windowWidth / 2) - (elementWidth / 2);
-                    let top = scrollTop + ((windowHeight / 2) - (elementHeight / 2));
-                    // bounding
-                    if (top < 0) top = 0;
-                    if (left < 0) left = 0;
-                    // set position
-                    element.style.top = `${top}px`;
-                    element.style.left = `${left}px`;
-                }
+            if (centerOnResize && innerRef.current) {
+                const element = innerRef.current;
+                const elementWidth = element.offsetWidth;
+                const elementHeight = element.offsetHeight;
+                const windowWidth = window.innerWidth;
+                const windowHeight = window.innerHeight;
+                const scrollTop = window.scrollY;
+                // calculate center
+                let left = (windowWidth / 2) - (elementWidth / 2);
+                let top = scrollTop + ((windowHeight / 2) - (elementHeight / 2));
+                // bounding
+                if (top < 0) top = 0;
+                if (left < 0) left = 0;
+                // set position
+                element.style.top = `${top}px`;
+                element.style.left = `${left}px`;
             }
         };
         if (show) callback();
         window.addEventListener('resize', callback);
         return () => window.removeEventListener('resize', callback);
-    }, [show, centerOnResize]);
-
-    useEffect(() => {
-        if (show) document.body.classList.add('modal-active');
-        else document.body.classList.remove('modal-active');
-    }, [show]);
+    }, [show, centerOnResize, innerRef]);
 
     return (
-        <>
-            <div id="modal-overlay" style={{ opacity: overlayOpacity, display: show ? "block" : "none" }} ref={overlayRef}></div>
+        <div id="modal" ref={ref}>
+            <div id="modal-overlay" style={{ display: isHidden ? "none" : "block", opacity: overlayOpacity }}></div>
             <div id="modal-loading"></div>
-            <div id="modal-window" ref={ref} style={{ visibility: show ? "visible" : "hidden", display: show ? "block" : "none" }}>
+            <div id="modal-window" ref={innerRef} style={{ visibility: isHidden ? "hidden" : "visible", display: isHidden ? "none" : "block" }}>
                 {closeModal && <a className="modal-close" href="#" onClick={e => {
                     e.preventDefault();
-                    fadeOutModal();
+                    closeModal();
                 }}>close</a>}
                 <div id="modal-content">{children}</div>
             </div>
-        </>
+        </div>
     );
 };
