e490b6e092af70392e262b40c4f786110343676c
benjamin <73396619+akbenjii@users.noreply.github.com>
2024-10-15 15:02:58 -0400

    implement system to generate animations from spritesheet

--- src/world/engine/clothing/clothingManager.ts
+++ src/world/engine/clothing/clothingManager.ts
@@ -7,6 +7,7 @@ import World from "@clubpenguin/world/World";
 import Phaser from "phaser";
 import { getLogger } from "@clubpenguin/lib/log";
 import { ItemType } from "./itemType";
+import { ActionFrame, ANIMATION_META } from "@clubpenguin/net/types/action";
 
 let logger = getLogger('CP.world.engine.clothing');
 
@@ -28,7 +29,6 @@ export class ClothingManager {
         this.engine.on('player:remove', (player: Player) => {
             for (let [_, clothing] of player.clothes) {
                 this.engine.cleaner.deallocateResource('multiatlas', this.getSpriteKey(clothing.config.paper_item_id), player.userData.id);
-                this.engine.cleaner.deallocateResource('json', this.getSpriteAnimationsKey(clothing.config.paper_item_id), player.userData.id);
             }
             this.engine.cleaner.collect();
         });
@@ -64,19 +64,18 @@ export class ClothingManager {
         return `clothing-sprites-${id}`;
     }
 
-    getSpriteAnimationsKey(id: number): string {
-        return `${this.getSpriteKey(id)}-animations`;
+    getSpriteAnimationKey(assetKey: string, action: number): string {
+        return `${assetKey}_${action}animation`;
     }
 
     async loadClothingSprite(config: PaperItemConfig, startLoading: boolean, playerId?: number): Promise<void> {
         let id = config.paper_item_id;
 
         let key = this.getSpriteKey(id);
-        let animationsKey = this.getSpriteAnimationsKey(id);
 
-        logger.info('Loading sprite', key, animationsKey);
+        logger.info('Loading sprite', key);
 
-        let loadSprite = new Promise<void>((resolve, reject) => {
+        await new Promise<void>((resolve, reject) => {
             if (this.world.textures.exists(key)) return resolve();
 
             this.world.load.multiatlas({
@@ -107,42 +106,6 @@ export class ClothingManager {
             this.world.load.on('loaderror', errorCallback);
         });
 
-        let loadAnimations = new Promise<void>((resolve, reject) => {
-            if (this.world.cache.json.exists(animationsKey)) return resolve();
-
-            this.world.load.json({
-                key: animationsKey,
-                url: `assets/clothing/sprites/${id}.anims.json`
-            });
-
-            let completeCallback = (key_: string, type_: string) => {
-                if (key_ == animationsKey && type_ == 'json') {
-                    this.world.load.off('filecomplete', completeCallback);
-                    this.world.load.off('loaderror', errorCallback);
-
-                    this.engine.cleaner.allocateResource(type_, key_, playerId);
-                    resolve();
-                }
-            }
-            this.world.load.on('filecomplete', completeCallback);
-
-            let errorCallback = (file: Phaser.Loader.File) => {
-                if (file.key == animationsKey && file.type == 'json') {
-                    this.world.load.off('filecomplete', completeCallback);
-                    this.world.load.off('loaderror', errorCallback);
-
-                    reject(new Error(`Animations for sprite ${id} failed to load!`));
-                }
-            }
-            this.world.load.on('loaderror', errorCallback);
-        });
-
-        if (startLoading) this.world.load.start();
-
-        await Promise.all([
-            loadSprite,
-            loadAnimations
-        ]);
     }
 
     async addClothingSprite(player: Player, type: ItemType, id: number, startLoading = true): Promise<ClothingSprite> {
@@ -150,10 +113,8 @@ export class ClothingManager {
             let clothing = player.clothes.get(type);
             if (clothing) {
                 let key = this.getSpriteKey(clothing.config.paper_item_id);
-                let animationsKey = this.getSpriteAnimationsKey(clothing.config.paper_item_id);
 
                 this.engine.cleaner.deallocateResource('multiatlas', key, player.userData.id);
-                this.engine.cleaner.deallocateResource('json', animationsKey, player.userData.id);
                 clothing.destroy();
                 player.clothes.delete(type);
             }
@@ -164,17 +125,14 @@ export class ClothingManager {
         logger.info('Adding sprite', config);
 
         let key = this.getSpriteKey(id);
-        let animationsKey = this.getSpriteAnimationsKey(id);
 
         for (let [slot, clothing] of player.clothes) {
             if (clothing.config.paper_item_id == id) return;
 
             let key = this.getSpriteKey(clothing.config.paper_item_id);
-            let animationsKey = this.getSpriteAnimationsKey(clothing.config.paper_item_id);
-    
+
             if (slot == config.type) {
                 this.engine.cleaner.deallocateResource('multiatlas', key, player.userData.id);
-                this.engine.cleaner.deallocateResource('json', animationsKey, player.userData.id);
                 clothing.destroy();
                 player.clothes.delete(slot);
                 break;
@@ -217,18 +175,17 @@ export class ClothingManager {
     }
 
     attachClothingSprite(player: Player, config: PaperItemConfig): ClothingSprite {
-        let key = this.getSpriteKey(config.paper_item_id);
-        let animationsKey = this.getSpriteAnimationsKey(config.paper_item_id);
+        let spriteKey = this.getSpriteKey(config.paper_item_id);
 
-        if (!this.world.textures.exists(key)) {
+        if (!this.world.textures.exists(spriteKey)) {
             logger.warn('Could not attach clothing (file missing)', player, config);
             return;
         }
 
-        let sprite = this.world.add.sprite(0, 0, key, `${config.paper_item_id}/0`) as ClothingSprite;
+        let sprite = this.world.add.sprite(0, 0, spriteKey, `${config.paper_item_id}/0`) as ClothingSprite;
         sprite.depth = this.getClothingDepth(config);
         sprite.config = config;
-        sprite.animations = this.createClothingAnimations(animationsKey);
+        sprite.animations = this.createClothingAnimations(player, spriteKey);
 
         player.add(sprite);
         player.sort('depth');
@@ -236,21 +193,112 @@ export class ClothingManager {
         return sprite;
     }
 
-    createClothingAnimations(key: string): { [frame: number]: Phaser.Animations.Animation; } {
-        if (!this.world.cache.json.exists(key)) return;
+    createClothingAnimations(player: Player, spriteKey: string): { [actionFrame: number]: Phaser.Animations.Animation } {
+        let animations: { [actionFrame: number]: Phaser.Animations.Animation } = {};
 
-        let data: Phaser.Types.Animations.JSONAnimations & { anims: (Phaser.Types.Animations.JSONAnimation & { index: string })[] } = this.world.cache.json.get(key);
-        let animations: { [frame: number]: Phaser.Animations.Animation; } = {};
+        const frameKeys = Object.keys(this.world.textures.get(spriteKey).frames);
+        const sortedFrameKeys = frameKeys.filter(frameKey => frameKey !== "__BASE");
 
-        for (let anim of data.anims) {
-            let animation = this.world.anims.exists(anim.key) ? this.world.anims.get(anim.key) : this.world.anims.create(anim);
+        sortedFrameKeys.sort((a, b) => {
+            const frameA = this.parseFrame(a);
+            const frameB = this.parseFrame(b);
 
-            if (animation != false) {
-                let frameIndex = parseInt(anim.index);
-                animations[frameIndex] = animation;
-            } else logger.warn(`Animation ${anim.key} failed to be created. Skipping.`);
+            return frameA.actionFrame === frameB.actionFrame
+                ? frameA.subframe - frameB.subframe
+                : frameA.actionFrame - frameB.actionFrame;
+        });
+
+        let animationFrames: { [actionFrame: number]: { frames: Phaser.Types.Animations.AnimationFrame[], itemId: number } } = {  };
+
+        for (const frameKey of sortedFrameKeys) {
+            const {itemId, actionFrame, subframe} = this.parseFrame(frameKey);
+            const isNested = subframe !== 0;
+
+            if (!(actionFrame in animationFrames)) {
+                animationFrames[actionFrame] = {
+                    frames: [],
+                    itemId: itemId
+                };
+            }
+
+            if (!isNested) {
+                this.addFrame(animationFrames[actionFrame].frames, spriteKey, `${itemId}/${actionFrame}`);
+
+                animationFrames[actionFrame].itemId = itemId;
+
+                continue;
+            }
+
+            this.addFrame(animationFrames[actionFrame].frames, spriteKey, `${itemId}/${actionFrame};${subframe}`);
+        }
+
+        for (const actionFrameId in animationFrames) {
+            const animationKey = this.getSpriteAnimationKey(spriteKey, parseInt(actionFrameId));
+
+            if (this.world.anims.exists(animationKey)) {
+                animations[actionFrameId] = this.world.anims.get(animationKey);
+                continue;
+            }
+
+            this.engine.cleaner.allocateResource('animation', animationKey, player.userData.id);
+
+            if (parseInt(actionFrameId) === ActionFrame.WAVE) {
+                const extraWaveFrames = {
+                    start: 5,
+                    end: 12,
+                    spriteKey,
+                    itemId: animationFrames[actionFrameId].itemId,
+                    action: ActionFrame.WAVE
+                }
+
+                this.addFrames(animationFrames[actionFrameId].frames, extraWaveFrames, 2);
+                this.addFrames(animationFrames[actionFrameId].frames, { ...extraWaveFrames, start: 1, end: 1 });
+            }
+
+            const animation = this.world.anims.create({
+                key: animationKey,
+                frames: animationFrames[actionFrameId].frames,
+                frameRate: 24,
+                skipMissedFrames: true,
+                repeat: ANIMATION_META[actionFrameId]?.repeat ? -1 : 0
+            });
+
+            if (!animation) {
+                logger.warn(`Animation ${animationKey} failed to be created. Skipping.`);
+                continue;
+            };
+
+            animations[actionFrameId] = animation;
         }
 
         return animations;
     }
+
+    parseFrame(frameKey: string): { itemId: number, actionFrame: number, subframe: number } {
+        const [itemId, tmp] = frameKey.split('/');
+        const [actionFrame, subframe = 0] = tmp.split(';').map(Number);
+
+        return { itemId: parseInt(itemId), actionFrame, subframe };
+    }
+
+    private addFrames(
+        frames: Phaser.Types.Animations.AnimationFrame[],
+        config: { start: number, end: number, spriteKey: string, itemId: number, action: ActionFrame },
+        repeat = 1
+    ) {
+        const { start, end, spriteKey, itemId, action } = config;
+
+        for (let r = 0; r < repeat; r++)
+            for (let j = start; j <= end; j++)
+                this.addFrame(frames, spriteKey, `${itemId}/${action};${j}`);
+    }
+
+    private addFrame(arr: Phaser.Types.Animations.AnimationFrame[], key: string, frame: string) {
+        return arr.push({
+            key,
+            frame,
+            duration: 0
+        });
+    }
+
 }
