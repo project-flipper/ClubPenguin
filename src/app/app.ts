ae183c3e4aa598a4d82dbdc84cdb435ac9878346
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-10-19 11:45:17 -0600

    Handle JSON and animation keys being shared when unloading

--- src/app/app.ts
+++ src/app/app.ts
@@ -121,17 +121,13 @@ export class App extends Phaser.Game {
      * @param assetKey The asset key associated.
      */
     unloadAnimation(assetKey: string): void {
-        if (!this.cache.json.exists(assetKey)) {
-            const isAnimationKey = this.anims.exists(assetKey);
-
-            if (isAnimationKey) this.anims.remove(assetKey);
-            return;
+        if (this.cache.json.exists(assetKey)) {
+            let animations = this.cache.json.get(assetKey) as Phaser.Types.Animations.JSONAnimations;
+            for (let animation of animations.anims) this.anims.remove(animation.key);
+    
+            this.cache.json.remove(assetKey);
         }
-
-        let animations = this.cache.json.get(assetKey);
-        for (let animation of animations.anims) this.anims.remove(animation.key);
-
-        this.cache.json.remove(assetKey);
+        if (this.anims.exists(assetKey)) this.anims.remove(assetKey);
     }
 
     /**
